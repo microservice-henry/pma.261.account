e308f01d5a4ddf5c6fa350e898456f31dc5fb7aa
HenryIdesis <henryi@al.insper.edu.br>
2026-04-22 13:03:34 -0300

    account

--- src/main/java/store/account/AccountController.java
+++ src/main/java/store/account/AccountController.java
@@ -1,30 +1,18 @@
 package store.account;
 
 import java.util.List;
-
 import org.springframework.cloud.openfeign.FeignClient;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.DeleteMapping;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.*;
 
-@FeignClient(
-    name="account",
-    url="http://account:8080"
-)
+@FeignClient(name = "account", url = "http://account:8080")
 public interface AccountController {
 
     @PostMapping("/accounts")
-    public ResponseEntity<Void> create(
-        @RequestBody AccountIn in
-    );
+    public ResponseEntity<AccountOut> create(@RequestBody AccountIn in);
 
     @DeleteMapping("/accounts/{id}")
-    public ResponseEntity<Void> delete(
-        @PathVariable String id
-    );
+    public ResponseEntity<Void> delete(@PathVariable String id);
 
     @GetMapping("/accounts/health-check")
     public ResponseEntity<Void> healthCheck();
@@ -33,9 +21,8 @@ public interface AccountController {
     public ResponseEntity<List<AccountOut>> findAll();
 
     @GetMapping("/accounts/{id}")
-    public ResponseEntity<AccountOut> findById(
-        @PathVariable String id
-    );
+    public ResponseEntity<AccountOut> findById(@PathVariable String id);
 
-    
+    @PostMapping("/accounts/login")
+    public ResponseEntity<AccountOut> findByEmailAndPassword(@RequestBody AccountIn in);
 }
